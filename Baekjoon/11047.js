8087ee7dba1d1ce8c27dcc2ecca76dba2283d28e
jooyong-boo <qnwndyd159@naver.com>
2022-09-13 00:45:39 +0900

    Algorithm

--- Baekjoon/11047.js
+++ Baekjoon/11047.js
@@ -1,12 +1,29 @@
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
-let input = fs.readFileSync(filePath).toString().split("\n");
+let input = fs.readFileSync(filePath).toString().trim().split("\n");
 
-const totalCoin = +input.shift().split(" ")[1];
-const coin = input.filter((item) => +totalCoin > +item);
+let totalCoin = Number(input[0].split(" ")[1]);
+const coin = input
+    .filter((item) => +totalCoin >= +item)
+    .sort((a, b) => b - a)
+    .map((item) => +item);
 
 let result = 0;
-while (result !== NK[1]) {
-    if ()
+for (let i = 0; i < coin.length; i++) {
+    if (totalCoin < coin[i]) {
+        continue;
+    }
+    if (totalCoin % coin[i] >= 0) {
+        result += Math.floor(totalCoin / coin[i]);
+        totalCoin -= coin[i] * Math.floor(totalCoin / coin[i]);
+    }
+    if (totalCoin === 0) {
+        break;
+    }
 }
-console.log(totalCoin);
+console.log(result);
+
+// 1. 동전을 내림차순으로 정렬
+// 2. totalCoin에서 coin[i]를 나눈 나머지가 0보다 크거나 같으면 result에 둘을 나눈 값을 내림하여 더하고
+// totalCoin에서 해당 값만큼 뺴준다
+// 3. totalCoin보다 coin[i]의 값이 크면 continue하고 totalCoin의 값이 0이면 반복문을 종료한다.
--- Baekjoon/11399.js
+++ Baekjoon/11399.js
@@ -20,3 +20,8 @@ for (let i = 0; i < people; i++) {
 }
 
 console.log(result);
+
+// 1. 걸리는 시간을 오름차순으로 정렬
+// 2. 결과값을 담을 result와 배열을 돌때마다 이전값들을 저장할 save 변수를 선언
+// 3. 사람의 수(people)만큼 for문을 실행하여 초기에 save 값이 0일때는 result에 time만 더해주고
+// 이후부터는 save 값을 result에 더해준다
