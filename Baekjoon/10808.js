1fff0ffa956313913836698f1d11e91450795ebf
jooyong-boo <qnwndyd159@naver.com>
2023-01-26 02:32:06 +0900

    알파벳 개수

--- Baekjoon/10808.js
+++ Baekjoon/10808.js
@@ -2,8 +2,6 @@ const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 let input = fs.readFileSync(filePath).toString().trim();
 
-console.log(input[1]);
-
 const result = new Array(26).fill(0);
 
 for (let i = 0; i < input.length; i++) {
